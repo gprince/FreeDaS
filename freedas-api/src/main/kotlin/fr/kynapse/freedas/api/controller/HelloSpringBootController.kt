751f587f22ee42cee161b362d827137abafeff71
Gregory Prince <gprince.ext@orange.com>
2017-05-11 17:09:14 +0200

    [GPE][API][#5] initializing Swagger 2

--- freedas-api/src/main/kotlin/fr/kynapse/freedas/api/controller/HelloSpringBootController.kt
+++ freedas-api/src/main/kotlin/fr/kynapse/freedas/api/controller/HelloSpringBootController.kt
@@ -1,5 +1,7 @@
 package fr.kynapse.freedas.api.controller
 
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,10 +12,16 @@ import org.springframework.web.bind.annotation.ResponseBody
  * <h1>Simple bootstrapped Hello Controller :: Just for Fun :D</h1>
  */
 @Controller
-@RequestMapping("/hello")
+@RequestMapping(value = "/sayHello")
+@Api(value = "Hello from FreeDaS", tags = arrayOf("Hello from FreeDaS"), description = "Simple foo Controller to say Hello from FreeDaS :smile:")
 class HelloSpringBootController {
 
-    @RequestMapping( value = "/{name}", method = arrayOf(GET))
+    @RequestMapping(value = "/{name}", method = arrayOf(GET))
     @ResponseBody
-    fun hello(@PathVariable("name") name: String) = "Hello $name"
+    @ApiOperation(
+            value = "Say Hello",
+            response = String::class,
+            tags = arrayOf("hello", "fun")
+    )
+    fun sayHello(@PathVariable("name") name: String) = "Hello $name from FreeDaS"
 }
